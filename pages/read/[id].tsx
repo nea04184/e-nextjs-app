d19d4f45ac68c6aae424bf4335b277ddf282ef85
nea04184 <51044545+nea04184@users.noreply.github.com>
2023-05-23 14:39:26 +0900

    Add files via upload

---
+++ pages/read/[id].tsx
@@ -0,0 +1,14 @@
+import Layout from "@/components/Layout";
+import Head from "next/head";
+
+export default function Read() {
+  return (
+    <>
+      <Head>
+        <title>WEB!</title>
+      </Head>
+      <h2>Read!</h2>
+      Hello, Read!
+    </>
+  );
+}
